5616bc8870d7cbe8806547549e1027e3984e64df
atulpatel991928 <75467319+atulpatel991928@users.noreply.github.com>
2021-08-01 23:17:58 +0530

    Add files via upload

---
+++ sorted_order_country_in_list.py
@@ -0,0 +1,9 @@
+dict = {}
+dict['Newzealand']= 'wellington'
+dict['India'] = 'New Delhi'
+dict['France']='Peris'
+dict['Russia']='Mascoo'
+dict['Australia'] ='kenbera'
+print(dict)
+for i in sorted (dict):
+    print( dict[i] , end=":")
